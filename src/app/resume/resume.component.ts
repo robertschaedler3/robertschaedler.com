0033f1dc29dbe41a5ccd95ede432814b0304de13
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-06 21:19:27 -0400

    update sidenav button style

---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+declare interface Panel {
+    title: string;
+    level: string;
+    description: string;
+    chips: Chip[];
+}
+
+declare interface Chip {
+    link: string;
+    name: string;
+}
+
+declare interface SocialItem {
+    name: string;
+    url: string;
+}
+
+
+
+@Component({
+    selector: 'app-resume',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent implements OnInit {
+
+    panels: Panel[] = [
+        {
+            title: 'Python',
+            level: 'Advanced',
+            description: 'I have used python for a wide variety of different projects. From data science and network security analysis to RESTful APIs I\'ve done it all',
+            chips: [
+                { link: '', name: 'numpy' },
+                { link: '', name: 'pyshark' },
+                { link: '', name: 'pandas' },
+                { link: '', name: 'Flask' },
+
+            ]
+        },
+        {
+            title: 'Java',
+            level: 'Intermediate',
+            description: '',
+            chips: [
+                { link: '', name: 'Data Structures' },
+                { link: '', name: 'OOP' },
+                { link: '', name: 'Obj. Oriented Software Engineering' },
+            ]
+        },
+        {
+            title: 'TypeScript',
+            level: 'Advanced',
+            description: '',
+            chips: [
+                { link: '', name: 'Angular CLI' },
+                { link: '', name: 'React' },
+                { link: '', name: 'Express' },
+            ]
+        },
+        {
+            title: 'C++',
+            level: 'Advanced',
+            description: 'My main used for C++ have been in the world of IoT. Writing firmware for IoT devices that connect and stream data to the cloud.',
+            chips: [
+                { link: '', name: 'Particle' },
+                { link: '', name: 'Arduino' },
+            ]
+        },
+        {
+            title: 'NodeJS',
+            level: 'Advanced',
+            description: '',
+            chips: [
+                { link: '', name: 'TypeScript' },
+                { link: '', name: 'Express' },
+                { link: '', name: 'RESTful' },
+                { link: '', name: 'Parse Server' },
+            ]
+        },
+        {
+            title: 'HTML/CSS/Javascript',
+            level: 'Advanced',
+            description: '',
+            chips: [
+                { link: '', name: 'Angular' },
+                { link: '', name: 'React' },
+                { link: '', name: 'Webpack' },
+            ]
+        },
+
+    ];
+
+    socialList: SocialItem[] = [
+        { name: "Github", url: "https://github.com/robertschaedler3" },
+        { name: "LinkedIn", url: "https://linkedin.com/in/robert-schaedler-iii/" },
+        { name: "Instagram", url: "https://www.instagram.com/robertschaedleriii/" }
+    ]
+
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
