8b4dd840fa8468127a2c6945194cadf268e5ff27
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-06 21:21:02 -0400

    create angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ResumeComponent } from './resume/resume.component'
+
+import { NotesLayoutComponent } from './layouts/notes-layout/notes-layout.component';
+
+const routes: Routes = [
+  { path: '', component: ResumeComponent },
+  { path: 'notes', redirectTo: 'notes', pathMatch: 'full' },
+  {
+    path: '',
+    component: NotesLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: './layouts/notes-layout/notes-layout.module#NotesLayoutModule'
+    }]
+  }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { CourseworkComponent } from './resume/coursework/coursework.component';
+import { DialogContent } from './resume/coursework/coursework.component';
+import { GithubComponent } from './resume/github/github.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+import { NotesLayoutComponent } from './layouts/notes-layout/notes-layout.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ResumeComponent,
+    CourseworkComponent,
+    DialogContent,
+    GithubComponent,
+    NotesLayoutComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatToolbarModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    DialogContent
   ],
   providers: [],
   bootstrap: [AppComponent]
