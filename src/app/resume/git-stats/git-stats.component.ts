a9847e6faa10341e2525c8df07ede6e19a9d5801
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-18 23:09:21 -0500

    display git stats

--- src/app/resume/git-stats/git-stats.component.ts
+++ src/app/resume/git-stats/git-stats.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { GitService } from './git.service';
 
+import { Contributions } from './git.service';
+
 @Component({
   selector: 'app-git-stats',
   templateUrl: './git-stats.component.html',
   styleUrls: ['./git-stats.component.scss']
 })
 export class GitStatsComponent implements OnInit {
+
+  today: number = Date.now();
 
   constructor(public git: GitService) { }
 
