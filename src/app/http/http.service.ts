aa3dc9472479cf9ed9c6731678f0e4bb963fb08f
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-18 19:51:47 -0500

    create request function

--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import * as _ from 'lodash';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  data$: Observable<any>;
+
+  constructor(private http: HttpClient) { }
+
+  public get(url: string) {
+    return this.data$ = this.http.get(url).pipe(
+      map(data => _.values(data)),
+      tap(console.log),
+    );
+  }
+
 }
