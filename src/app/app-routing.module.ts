9036692d0443e28f7e8af8bfde1c3b1cc3c68f68
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-05 14:56:33 -0400

    Merge pull request #6 from robertschaedler3/origin/robertschaedler3/refactoring
    Begin refactoring of stylesheets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ResumeComponent } from './resume/resume.component'
+import { ResumeLayoutComponent } from './resume-layout/resume-layout.component';
 
 
 const routes: Routes = [
   { path: '', component: ResumeComponent },
+  { path: 'resume', component: ResumeLayoutComponent },
   { path: '**', redirectTo: '/' },
 ];
 
---
+++ src/app/resume-layout/resume-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-layout',
+  templateUrl: './resume-layout.component.html',
+  styleUrls: ['./resume-layout.component.scss']
+})
+export class ResumeLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
