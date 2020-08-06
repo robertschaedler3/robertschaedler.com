77de0f8826c1d4a2be6f43948e1d798bc08338a8
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-06 13:53:44 -0400

    add image formatting

--- src/app/core/components/image.component.ts
+++ src/app/core/components/image.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image',
   template: `
-    <p>
-      image works!
-    </p>
-  `,
-  styles: []
+    <div class="thumbnail">
+      <a [href]="link">
+        <img [ngClass]="{
+          'large': size === 'large', 
+          'medium': size === 'medium', 
+          'small': size === 'small', 
+          'round': rounded
+        }" [src]="src" [alt]="alt">
+      </a>
+  </div>`,
+  styles: [
+    `.image-conainer {
+      min-height: 5rem;
+    },
+    .small {
+      width: 8rem;
+    }
+    .medium {
+      width: 12rem;
+    }
+    .large {
+      width: 18rem;
+    }
+    .round {
+      border-radius: 0.675rem;
+    }`
+  ]
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent {
+
+  // Image attributes
+  @Input() link: string = '#';
+  @Input() src: string;
+  @Input() alt: string;
+
+  // Style options
+  @Input() size: 'small' | 'medium' | 'large';
+  @Input() rounded: boolean = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
