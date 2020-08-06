635c663a8798c3b9b91ccf3f1d03799e3b295fbb
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-06 14:59:48 -0400

    add resume-section template

--- src/app/core/components/section.component.ts
+++ src/app/core/components/section.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   template: `
-  <div class="section">
+  <div class="section" [ngClass]="{'padd-top': paddTop, 'padd-bottom': paddBottom}">
     <div class="container">
         <ng-content></ng-content>
     </div>
@@ -12,14 +12,23 @@ import { Component } from '@angular/core';
     `.section {
       width: 100%;
       background-color: inherit;
-      padding: 8rem 0rem;
+      padding: 0rem;
     }
     .container {
       padding: 0rem 2rem;
+    }
+    .padd-top {
+      padding-top: 8rem !important;
+    }
+    .padd-bottom {
+      padding-bottom: 8rem !important;
     }`
   ]
 })
 export class SectionComponent {
+
+  @Input() paddTop: boolean = true;
+  @Input() paddBottom: boolean = true;
 
   constructor() { }
 
--- src/app/resume-layout/resume-section/resume-section.component.ts
+++ src/app/resume-layout/resume-section/resume-section.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resume-section',
   templateUrl: './resume-section.component.html',
   styleUrls: ['./resume-section.component.scss']
 })
-export class ResumeSectionComponent implements OnInit {
+export class ResumeSectionComponent {
+
+  @Input() title: string;
+  @Input() icon: string;
+
+  @Input() paddTop: boolean = true;
+  @Input() paddBottom: boolean = true;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
