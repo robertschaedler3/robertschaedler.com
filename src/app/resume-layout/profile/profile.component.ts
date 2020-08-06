b543915383eaad36facba025f3d14891f7d30f2f
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-06 13:55:00 -0400

    add profile content

--- src/app/resume-layout/profile/profile.component.ts
+++ src/app/resume-layout/profile/profile.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Chip } from 'src/app/core/components/chip-list.component';
+
+export const SOCIAL_CHIPS: Chip[] = [
+  { name: "Github", url: "https://github.com/robertschaedler3" },
+  { name: "LinkedIn", url: "https://linkedin.com/in/robert-schaedler-iii/" },
+  { name: "Instagram", url: "https://www.instagram.com/robertschaedleriii/" },
+  { name: "Medium", url: "https://medium.com/@robertschaedler3" }
+];
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+
+  chips: Chip[] = SOCIAL_CHIPS;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
