1daa0869373e0f84f3f169060ad7c5283e0d88c8
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-06 11:41:11 -0400

    create image component

---
+++ src/app/core/components/image.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  template: `
+    <p>
+      image works!
+    </p>
+  `,
+  styles: []
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ImageComponent } from './components/image.component';
 import { SectionComponent } from './components/section.component';
 
 
 @NgModule({
   declarations: [
+    ImageComponent,
     SectionComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    ImageComponent,
     SectionComponent
   ]
 })
