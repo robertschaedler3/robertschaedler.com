4c325df8bc5952e9e0660625cd4aa5ce8223b835
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-05 20:48:35 -0400

    move material imports to shard module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// TODO: remove unused imports
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+  ], exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+  ]
+})
+export class MaterialModule { }
