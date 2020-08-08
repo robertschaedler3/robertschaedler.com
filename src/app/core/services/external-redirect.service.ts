b3eee7c91b63d9ec460c5cb8299e2be116ed5d5d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-08 01:58:49 -0400

    add function for opening a redirect in new window

--- src/app/core/services/external-redirect.service.ts
+++ src/app/core/services/external-redirect.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class ExternalRedirectService {
 
   constructor() { }
+
+  open(url: string) {
+    window.open(url);
+  }
 }
--- src/app/resume-layout/header/header.component.ts
+++ src/app/resume-layout/header/header.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ExternalRedirectService } from 'src/app/core/services/external-redirect.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  // TODO: move to service
-  goToLink(url: string) {
-    window.open(url, "_blank");
-  }
+  constructor(public redirect: ExternalRedirectService) { }
 
 }
