2a0d355774415e124151f16bbf40b68d2668210d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-11 14:32:42 -0400

    create core header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { ChipListComponent } from './components/chip-list.component';
+import { HeaderComponent } from './components/header.component';
 import { ImageComponent } from './components/image.component';
 import { SectionComponent } from './components/section.component';
-import { ChipListComponent } from './components/chip-list.component';
 import { MaterialModule } from '../shared/material.module';
 
 
 @NgModule({
   declarations: [
     ChipListComponent,
+    HeaderComponent,
     ImageComponent,
     SectionComponent
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     ChipListComponent,
+    HeaderComponent,
     ImageComponent,
     SectionComponent
   ]
