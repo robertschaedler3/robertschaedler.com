dc76078ea1dfd6039913b173b345407625b20e07
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-17 02:10:48 -0500

    add page routes

--- apps/ng-app/src/app/modules/resume/resume-routing.module.ts
+++ apps/ng-app/src/app/modules/resume/resume-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResumeComponent } from './resume.component';
 
-const routes: Routes = [{ path: '', component: ResumeComponent }];
+// Pages
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ResumeComponent,
+    children: [
+      { path: 'about', component: AboutPageComponent },
+      { path: 'experience', component: ExperiencePageComponent },
+      { path: 'projects', component: ProjectsPageComponent },
+      { path: 'skills', component: SkillsPageComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
