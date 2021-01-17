9077833d56c8e31e8511b9c279afad45039afec4
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-17 01:49:06 -0500

    add footer content

--- apps/ng-app/src/app/modules/resume/components/footer/footer.component.ts
+++ apps/ng-app/src/app/modules/resume/components/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  now = new Date();
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
