45d64e03f527c7ce856af9608046035ee1116c47
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-17 01:32:22 -0500

    add sidebar content

--- apps/ng-app/src/app/modules/resume/components/sidebar/sidebar.component.ts
+++ apps/ng-app/src/app/modules/resume/components/sidebar/sidebar.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface RouteInfo {
+  path: string;
+  title: string;
+  class: string;
+}
+export interface SocialLink {
+  link: string;
+  icon: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  { path: '/experience', title: 'Experience', class: '' },
+  { path: '/projects', title: 'Projects', class: '' },
+  { path: '/skills', title: 'Skills', class: '' },
+  { path: '/about', title: 'About', class: '' },
+];
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    link: 'https://github.com/robertschaedler3',
+    icon: 'github',
+  },
+  {
+    link: 'https://linkedin.com/in/robert-schaedler-iii/',
+    icon: 'linkedin-in',
+  },
+  {
+    link: 'https://medium.com/@robertschaedler3',
+    icon: 'medium-m',
+  },
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  routeItems: RouteInfo[];
+  socialLinks: SocialLink[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.routeItems = ROUTES.filter((menuItem) => menuItem);
+    this.socialLinks = SOCIAL_LINKS.filter((socialLink) => socialLink);
   }
-
 }
