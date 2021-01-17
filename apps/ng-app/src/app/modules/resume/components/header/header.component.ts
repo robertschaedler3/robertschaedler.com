771e6c3add64ab6021768eb99d5a2c64c53d6344
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-17 01:54:10 -0500

    add header content

--- apps/ng-app/src/app/modules/resume/components/header/header.component.ts
+++ apps/ng-app/src/app/modules/resume/components/header/header.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { ROUTES, RouteInfo } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  private routeItems: RouteInfo[];
+  private location: Location;
 
-  ngOnInit(): void {
+  constructor(location: Location) {
+    this.location = location;
   }
 
+  ngOnInit(): void {
+    this.routeItems = ROUTES.filter((route) => route);
+  }
+
+  getTitle() {
+    let title = this.location.prepareExternalUrl(this.location.path());
+    if (title.charAt(0) === '#') {
+      title = title.slice(1);
+    }
+
+    for (const routeItem of this.routeItems) {
+      if (routeItem.path === title) {
+        return routeItem.title;
+      }
+    }
+
+    return '';
+  }
 }
