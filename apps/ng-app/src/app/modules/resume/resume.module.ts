4723189eec26743c735033fd772965043080ac72
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-23 16:04:00 -0500

    create project-section component

--- apps/ng-app/src/app/modules/resume/resume.module.ts
+++ apps/ng-app/src/app/modules/resume/resume.module.ts
@@ -13,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { ProjectSectionComponent } from './components/project-section/project-section.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SocialLinkComponent } from './components/social-link/social-link.component';
 import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
@@ -26,6 +27,7 @@ import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
     HeaderComponent,
     LandingPageComponent,
     ProjectsPageComponent,
+    ProjectSectionComponent,
     ResumeComponent,
     SidebarComponent,
     SkillsPageComponent,
