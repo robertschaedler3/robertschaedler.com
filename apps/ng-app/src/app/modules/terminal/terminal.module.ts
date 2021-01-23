38cc9ae1f0e04dda36c0c1fc826df1413d579592
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-22 21:50:15 -0500

    create terminal module

---
+++ apps/ng-app/src/app/modules/terminal/terminal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TerminalRoutingModule } from './terminal-routing.module';
+import { TerminalComponent } from './terminal.component';
+
+
+@NgModule({
+  declarations: [TerminalComponent],
+  imports: [
+    CommonModule,
+    TerminalRoutingModule
+  ]
+})
+export class TerminalModule { }
