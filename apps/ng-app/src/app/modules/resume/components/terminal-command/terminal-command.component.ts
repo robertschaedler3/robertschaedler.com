3044adf1f1bda99e242f5819797e57a2121ebb3c
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-22 21:09:36 -0500

    create terminal-command component

---
+++ apps/ng-app/src/app/modules/resume/components/terminal-command/terminal-command.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal-command',
+  templateUrl: './terminal-command.component.html',
+  styleUrls: ['./terminal-command.component.scss']
+})
+export class TerminalCommandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
