82c9fc439ab87601d36b55ecec5604cbbce02c75
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-22 21:50:50 -0500

    exec commands

--- apps/ng-app/src/app/core/services/command.service.ts
+++ apps/ng-app/src/app/core/services/command.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface Command {
+  cmd: string;
+  result: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommandService {
 
+  private command = new Subject<Command>();
+  $command = this.command.asObservable();
+
   constructor() { }
+
+  public exec(command: string) {
+    console.log(`Executing: "${command}"`);
+
+    this.command.next({
+      cmd: command,
+      result: 'Command executed!'
+    });
+  }
+
 }
