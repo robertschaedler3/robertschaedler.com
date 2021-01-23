090783f8fd32543dac0927a756f4d6d67f495849
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-22 21:51:55 -0500

    display terminal results with command

--- apps/ng-app/src/app/modules/resume/components/terminal-command/terminal-command.component.ts
+++ apps/ng-app/src/app/modules/resume/components/terminal-command/terminal-command.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Command, CommandService } from '../../../../core/services/command.service';
+
 
 @Component({
   selector: 'app-terminal-command',
   templateUrl: './terminal-command.component.html',
-  styleUrls: ['./terminal-command.component.scss']
+  styleUrls: ['./terminal-command.component.scss'],
 })
 export class TerminalCommandComponent implements OnInit {
 
-  constructor() { }
+  @Input() command: Command;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
