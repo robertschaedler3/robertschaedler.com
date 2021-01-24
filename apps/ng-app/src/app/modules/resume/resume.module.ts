6b1bf2df42ede951fc24a7de2cee80a8ca91f707
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-24 13:31:03 -0500

    create experience component

--- apps/ng-app/src/app/modules/resume/resume.module.ts
+++ apps/ng-app/src/app/modules/resume/resume.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from '../../shared/shared.module';
 // Components
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ArchivePageComponent } from './pages/archive-page/archive-page.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -22,6 +23,7 @@ import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
   declarations: [
     AboutPageComponent,
     ArchivePageComponent,
+    ExperienceComponent,
     ExperiencePageComponent,
     FooterComponent,
     HeaderComponent,
