bd32cf974c4263cd38ae27b6b387082327ddfc33
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-24 19:05:48 -0500

    add experience

--- apps/ng-app/src/app/modules/resume/components/experience/experience.component.ts
+++ apps/ng-app/src/app/modules/resume/components/experience/experience.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { WORK_EXPERIENCE } from '../../data';
+import { WorkExperience } from '../../models/resume.model';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  experiences: WorkExperience[];
+  selection: WorkExperience;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.experiences = WORK_EXPERIENCE;
+    this.selection = this.experiences[0];
   }
-
 }
