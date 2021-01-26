d7560e6e3e8832575a4e22e1381a50c4de4d2ff1
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-26 12:06:29 -0500

    create navlink component

---
+++ apps/ng-app/src/app/modules/resume/components/navlink/navlink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navlink',
+  templateUrl: './navlink.component.html',
+  styleUrls: ['./navlink.component.scss']
+})
+export class NavlinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
