791f8f798f2dfd357584fc82d3dd7f08813571af
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-25 22:26:32 -0500

    get projects from github api

--- apps/ng-app/src/app/modules/resume/components/projects/projects.component.ts
+++ apps/ng-app/src/app/modules/resume/components/projects/projects.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  $projects: Observable<Project[]>;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    this.$projects = this.http.get<Project[]>(
+      'https://api.github.com/users/robertschaedler3/repos',
+      {
+        headers: {
+          Accept: 'application/vnd.github.mercy-preview+json',
+        },
+        params: {
+          sort: 'pushed',
+          per_page: '6',
+        },
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
